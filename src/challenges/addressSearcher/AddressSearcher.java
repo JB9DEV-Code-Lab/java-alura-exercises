7a0289c8bc9a36d6f0fcb73e98ee380ed014d3f6
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-10-06 23:23:06 +0100

    feat: use SearchedAddressRepository to access json file

--- src/challenges/addressSearcher/AddressSearcher.java
+++ src/challenges/addressSearcher/AddressSearcher.java
@@ -7,6 +7,7 @@ import utils.RunnableMenuOption;
 
 public class AddressSearcher extends RunnableMenuOption {
     private final Reader reader = new Reader();
+
     public AddressSearcher() {
         super("Address searcher");
     }
@@ -14,15 +15,15 @@ public class AddressSearcher extends RunnableMenuOption {
     @Override
     public void run() {
         System.out.println("""
-        ################################################################################
-        Welcome to Address Searcher!
-        
-        In this application you're going to be able to search brazilian addresses using
-        zip code. It's straight and simple, just type the zip code and it's going to
-        answer the found address or a message if it couldn't be found. Well, that's it,
-        so let's start!
-        ################################################################################
-        """);
+                ################################################################################
+                Welcome to Address Searcher!
+                        
+                In this application you're going to be able to search brazilian addresses using
+                zip code. It's straight and simple, just type the zip code and it's going to
+                answer the found address or a message if it couldn't be found. Well, that's it,
+                so let's start!
+                ################################################################################
+                """);
 
         String zipcode = reader.askForInput("What's the zip code?");
 
@@ -30,9 +31,9 @@ public class AddressSearcher extends RunnableMenuOption {
             AddressHttpService.searchZipcode(zipcode);
         } else {
             System.out.println("""
-            Exiting from application since no zip code was provided.
-            If you want to search for a new one, restart the application.
-            """);
+                    Exiting from application since no zip code was provided.
+                    If you want to search for a new one, restart the application.
+                    """);
 
             System.exit(0);
         }
--- src/challenges/addressSearcher/services/AddressHttpService.java
+++ src/challenges/addressSearcher/services/AddressHttpService.java
@@ -2,6 +2,7 @@ package challenges.addressSearcher.services;
 
 import challenges.addressSearcher.dtos.AddressDTO;
 import challenges.addressSearcher.models.Address;
+import challenges.addressSearcher.repository.SearchedAddressRepository;
 import com.google.gson.Gson;
 
 import java.io.IOException;
@@ -13,26 +14,36 @@ import java.net.http.HttpResponse;
 public class AddressHttpService {
     public static void searchZipcode(String zipcode) {
         try {
+            String addressZipcodeFoundTemplate = "%nThe address for this zip code is %s";
             String endpoint = String.format(
                     "https://viacep.com.br/ws/%s/json/",
                     zipcode.replaceAll("\\D", "")
             );
 
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endpoint)).build();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            SearchedAddressRepository searchedAddressRepository = new SearchedAddressRepository();
+            Address addressBYZipcode = searchedAddressRepository.getSearchedByZipcode(zipcode);
 
-            if (response.statusCode() >= 200 && response.statusCode() < 300) {
-                Gson gson = new Gson();
-                AddressDTO addressDTO = gson.fromJson(response.body(), AddressDTO.class);
-                Address address = new Address(addressDTO);
+            if (addressBYZipcode == null) {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endpoint)).build();
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-                System.out.printf("The address for this zip code is %s", address);
+                if (response.statusCode() >= 200 && response.statusCode() < 300) {
+                    Gson gson = new Gson();
+                    AddressDTO addressDTO = gson.fromJson(response.body(), AddressDTO.class);
+                    Address address = new Address(addressDTO);
+                    System.out.printf(addressZipcodeFoundTemplate, address);
+
+                    searchedAddressRepository.saveAddress(address);
+                } else {
+                    System.out.printf("%nCouldn't find any address for %s", zipcode);
+                }
             } else {
-                System.out.printf("Couldn't find any address for %s", zipcode);
+                System.out.printf(addressZipcodeFoundTemplate, addressBYZipcode);
             }
+
         } catch (IOException | InterruptedException exception) {
-            System.out.printf("Had a problem to search for %s. Error message: %s", zipcode, exception.getMessage());
+            System.out.printf("%nHad a problem to search for %s. Error message: %s", zipcode, exception.getMessage());
         }
     }
 }
